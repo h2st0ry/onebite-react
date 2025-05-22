85c41de25d68f6e6d7b5ac6d36973e3b451973bb
h2st0ry <yeonhee040512@gmail.com>
2025-05-22 23:56:48 +0900

    section03 - randomColor

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,6 +1,10 @@
 import mul from "./math.js";
 import { add, sub } from "./math.js";
+import randomColor from "randomcolor";
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
-console.log(mul(1, 3));
+const color = randomColor();
+console.log(color);
+
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(mul(1, 3));
