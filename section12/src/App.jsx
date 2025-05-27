4bc44cfef4d3b8da8c10b44537d13f528dbb7446
Hui._. <yeonhee040512@gmail.com>
2025-05-27 23:55:43 +0900

    section12 환경 설정

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react'
 import './App.css'
 
+// 1. "/" ; 모든 일기를 조회하는 Home 페이지
+// 2. "/new" : 새로운 일기를 작성하는 New 페이지
+// 3. "/diary" : 일기를 상세히 조회하는 Diary
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
