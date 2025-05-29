1556c2410335970d16ea4a978a2465307aa37a45
Hui._. <yeonhee040512@gmail.com>
2025-05-29 22:35:40 +0900

    section12 - Edit.jsx 수정 및 App.jsx -> mockData 설정

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,22 +1,34 @@
 import "./App.css";
-import { Routes, Route, Link, useNavigate, createPath } from "react-router-dom";
+import { useReducer } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
 import Notfound from "./pages/Notfound";
 import Edit from "./pages/Edit";
 
-import Button from "./components/Button";
-import Header from "./components/Header";
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  }
+]
 
-import { getEmotionImage } from "./util/get-emotion-image";
+function reducer(data, action) {
+  return state;
+}
 
-// 1. "/" ; 모든 일기를 조회하는 Home 페이지
-// 2. "/new" : 새로운 일기를 작성하는 New 페이지
-// 3. "/diary" : 일기를 상세히 조회하는 Diary
 
 function App() {
-  const nav = useNavigate();
+  const [data, dispatch] = useReducer(reducer, mockData);
 
   return (
     <>
