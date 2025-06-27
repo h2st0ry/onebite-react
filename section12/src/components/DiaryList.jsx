bb324111194711998d2a57726dccc7e230e4d9f5
Hui._. <yeonhee040512@gmail.com>
2025-06-27 22:34:51 +0900

    section12 - UPDATE DiaryList & ADD DiaryItem

--- section12/src/components/DiaryList.jsx
+++ section12/src/components/DiaryList.jsx
@@ -1,5 +1,19 @@
+import Button from "./Button";
+import "./DiaryList.css";
+
 const DiaryList = () => {
-    return <div>List</div>
+    return (
+        <div className="DiaryList">
+            <div className="menu_bar">
+                <select>
+                    <option value={"latest"}>최신순</option>
+                    <option value={"oldest"}>오래된 순</option>
+                </select>
+                <Button text={"새 일기 쓰기"} /> {/*type={"HAPPY"}로 하면 초기 설정 가능*/}
+            </div>
+            <div className="list_wrapper"></div>
+        </div>
+    )
 };
 
 export default DiaryList;
--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import Header from "../components/Header";
 import Button from "../components/Button";
+import DiaryList from "../components/DiaryList";
 
 const Home = () => {
   return (
     <div>
       <Header title={"2025년 6월"}
         leftChild={<Button text={"<"} />}
-        rightChild={<Button text={">"} />} />
+        rightChild={<Button text={">"} />}
+      />
+      <DiaryList />
     </div>
   );
 };
