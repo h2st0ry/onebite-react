525d717b44afaf31f90507e4ad8f5a58b71d4dc0
Hui._. <yeonhee040512@gmail.com>
2025-06-29 19:07:18 +0900

    section12 - UPDATE  Home

--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
 import Header from "../components/Header";
 import Button from "../components/Button";
 import DiaryList from "../components/DiaryList";
 
 const Home = () => {
+
+  const [pivoDate, setPivoDate] = useState(new Date());
+
+  const onIncreaseMonth = () => {
+    setPivoDate(new Date(pivoDate.getFullYear(), pivoDate.getMonth() + 1))
+  };
+
+  const onDecreaseMonth = () => {
+    setPivoDate(new Date(pivoDate.getFullYear(), pivoDate.getMonth() - 1))
+  };
+
   return (
     <div>
-      <Header title={"2025년 6월"}
-        leftChild={<Button text={"<"} />}
-        rightChild={<Button text={">"} />}
+      <Header
+        title={`${pivoDate.getFullYear()}년 ${pivoDate.getMonth() + 1}월`}
+        leftChild={<Button onClick={onDecreaseMonth} text={"<"} />}
+        rightChild={<Button onClick={onIncreaseMonth} text={">"} />}
       />
       <DiaryList />
     </div>
