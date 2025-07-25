ffaca0a80aab77027c848eece3a4fe08edde85d6
Hui._. <yeonhee040512@gmail.com>
2025-07-25 15:52:06 +0900

    section12 - UPDATE Edit -> nav

--- section12/src/pages/Edit.jsx
+++ section12/src/pages/Edit.jsx
@@ -1,9 +1,22 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
 
 const Edit = () => {
     const params = useParams();
+    const nav = useNavigate();
 
-    return <div>{params.id}번 일기 수정 페이지입니다.</div>
-}
+    return (
+        <div>
+            <Header
+                title={"일기 수정하기"}
+                leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />}
+                rightChild={<Button text={"삭제하기"} type={"NEGATIVE"} />}
+            />
+            <Editor />
+        </div>
+    )
+};
 
 export default Edit;
